5476357d2194a7f0131e4625828a5fc11b929b3b
Christopher Austin <66553323+Data-Curry@users.noreply.github.com>
2023-08-18 23:48:49 +0900

    Initial main menu options and database SQL querries work.

---
+++ app.py
@@ -0,0 +1,91 @@
+import os
+import psycopg2
+from psycopg2.errors import DivisionByZero
+from dotenv import load_dotenv
+import database
+
+
+DATABASE_PROMPT = "Enter the DATABASE_URI value or leave empty to load from .env file: "
+MENU_PROMPT = """-- Menu --
+
+1) Create new election
+2) List open elections
+3) Vote in an election
+4) Show election votes
+5) Exit
+
+Enter your choice: """
+NEW_CANDIDATE_PROMPT = "Enter new candidate's name (or leave empty to stop adding candidates): "
+
+
+def prompt_create_election(connection):
+    election_title = input("Enter election title: ")
+    election_creator = input("Enter election creator: ")
+    candidates = []
+
+    while (new_candidate := input(NEW_CANDIDATE_PROMPT)):
+        candidates.append(new_candidate)
+
+    database.create_election(connection, election_title, election_creator, candidates)
+
+
+def list_open_elections(connection):
+    elections = database.get_elections(connection)
+
+    for _id, title, creator in elections:
+        print(f"{_id}: {title} (created by {creator})")
+
+
+def prompt_vote_election(connection):
+    election_id = int(input("Enter election would you like to vote on: "))
+
+    election_candidates = database.get_election_details(connection, election_id)
+    _print_election_candidates(election_candidates)
+
+    candidate_id = int(input("Enter candidate you'd like to vote for: "))
+    username = input("Enter the username you'd like to vote as: ")
+    database.add_election_vote(connection, username, candidate_id)
+
+
+def _print_election_candidates(election_with_candidates):
+    for candidate in election_with_candidates:
+        print(f"{candidate[3]}: {candidate[4]}")
+
+
+def show_election_votes(connection):
+    election_id = int(input("Enter election you would like to see votes for: "))
+    try:
+        # This gives us count and percentage of votes for each candidate in an election
+        election_and_votes = database.get_election_and_vote_results(connection, election_id)
+    except DivisionByZero:
+        print("No votes have been cast in this election.")
+    else:
+        for _id, candidate_text, count, percentage in election_and_votes:
+            print(f"{candidate_text} got {count} votes ({percentage:.2f}% of total)")
+
+
+MENU_OPTIONS = {
+    "1": prompt_create_election,
+    "2": list_open_elections,
+    "3": prompt_vote_election,
+    "4": show_election_votes
+}
+
+
+def menu():
+    database_uri = input(DATABASE_PROMPT)
+    if not database_uri:
+        load_dotenv()
+        database_uri = os.environ["DATABASE_URI"]
+
+    connection = psycopg2.connect(database_uri)
+    database.create_tables(connection)
+
+    while (selection := input(MENU_PROMPT)) != "5":
+        try:
+            MENU_OPTIONS[selection](connection)
+        except KeyError:
+            print("Invalid input selected. Please try again.")
+
+
+menu()
---
+++ database.py
@@ -0,0 +1,73 @@
+from psycopg2.extras import execute_values
+
+
+CREATE_ELECTIONS = """CREATE TABLE IF NOT EXISTS elections
+(id SERIAL PRIMARY KEY, title TEXT, creator_username TEXT);"""
+CREATE_CANDIDATES = """CREATE TABLE IF NOT EXISTS candidates
+(id SERIAL PRIMARY KEY, candidate_text TEXT, election_id INTEGER, FOREIGN KEY(election_id) REFERENCES elections (id));"""
+CREATE_VOTES = """CREATE TABLE IF NOT EXISTS votes
+(username TEXT, candidate_id INTEGER, FOREIGN KEY(candidate_id) REFERENCES candidates (id));"""
+
+
+SELECT_ALL_ELECTIONS = "SELECT * FROM elections;"
+SELECT_ELECTION_WITH_CANDIDATES = """SELECT * FROM elections
+JOIN candidates ON elections.id = candidates.election_id
+WHERE elections.id = %s;"""
+SELECT_ELECTION_VOTE_DETAILS = """SELECT 
+candidates.id, 
+candidates.candidate_text, 
+COUNT(votes.candidate_id) AS vote_count, 
+COUNT(votes.candidate_id) / SUM(COUNT(votes.candidate_id)) OVER() * 100.0 AS vote_percentage 
+FROM candidates 
+LEFT JOIN votes ON candidates.id = votes.candidate_id 
+WHERE candidates.election_id = %s 
+GROUP BY candidates.id;"""
+
+INSERT_ELECTION_RETURN_ID = "INSERT INTO elections (title, creator_username) VALUES (%s, %s) RETURNING id;"
+INSERT_CANDIDATE = "INSERT INTO candidates (candidate_text, election_id) VALUES %s;"
+INSERT_VOTE = "INSERT INTO votes (username, candidate_id) VALUES (%s, %s);"
+
+
+def create_tables(connection):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_ELECTIONS)
+            cursor.execute(CREATE_CANDIDATES)
+            cursor.execute(CREATE_VOTES)
+
+
+def get_elections(connection):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ALL_ELECTIONS)
+            return cursor.fetchall()
+
+
+def get_election_details(connection, election_id):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ELECTION_WITH_CANDIDATES, (election_id,))
+            return cursor.fetchall()
+
+
+def get_election_and_vote_results(connection, election_id):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ELECTION_VOTE_DETAILS, (election_id,))
+            return cursor.fetchall()
+
+def create_election(connection, title, creator, candidates):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_ELECTION_RETURN_ID, (title, creator))
+
+            election_id = cursor.fetchone()[0]
+            candidate_values = [(candidate_text, election_id) for candidate_text in candidates]
+
+            execute_values(cursor, INSERT_CANDIDATE, candidate_values)
+
+
+def add_election_vote(connection, username, candidate_id):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_VOTE, (username, candidate_id))
